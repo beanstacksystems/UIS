3a9b902754d11a296a24226994cad236b9b99335
Amit <mail2amit.saha@gmail.com>
2021-03-27 21:35:01 +0530

    changes for facebook login

---
+++ Readme.md
@@ -0,0 +1,57 @@
+https://developers.facebook.com/docs/facebook-login/android
+
+https://stackoverflow.com/questions/7506392/how-to-create-android-facebook-key-hash
+
+Here is complete details (For Windows)
+
+1. Download OpenSSl either 3rd or 4th (with e will work better) based on your system 32bit or 64bit .
+
+2. Extract the downloaded zip inside C directory
+
+3. Open the extracted folder up to bin and copy the path ,it should be some thing like C:\openssl-0.9.8k_X64\bin\openssl (add \openssl at end)
+
+4. (Get the path to the bin folder of Jdk ,if you know how,ignore this ) .
+
+Open android studio ~file~Project Structure(ctrl+alt+shift+s) , select SDK location in left side panel ,copy the JDK location and add /bin to it
+
+So final JDK Location will be like C:\Program Files\Android\Android Studio\jre\bin
+
+we are following this method to get Jdk location because you might use embedded jdk like me
+
+enter image description here
+
+now you have OpenSSl location & JDK location
+
+5. now we need debug keystore location , for that open C~>Users~>YourUserName~>.android there should be a file name debug.keystore ,now copy the path location ,it should be some thing like
+
+C:\Users\Redman\.android\debug.keystore
+
+6. now open command prompt and type command
+
+cd YourJDKLocationFromStep4
+in my case
+
+ cd "C:\Program Files\Android\Android Studio\jre\bin"
+7. now construct the following command
+
+keytool -exportcert -alias androiddebugkey -keystore YOURKEYSTORELOCATION | YOUROPENSSLLOCATION sha1 -binary | YOUROPENSSLLOCATION base64
+in my case the command will look like
+
+keytool -exportcert -alias androiddebugkey -keystore "C:\Users\Redman\.android\debug.keystore" | "C:\openssl-0.9.8k_X64\bin\openssl" sha1 -binary | "C:\openssl-0.9.8k_X64\bin\openssl" base64
+now enter this command in command prompt , if you did ever thing right you will be asked for password (password is android)
+
+Enter keystore password:  android
+thats it ,you will be given the Key Hash , just copy it and use it
+
+For Signed KeyHash construct the following Command
+
+keytool -exportcert -alias YOUR_ALIAS_FOR_JKS -keystore YOUR_JKS_LOCATION | YOUROPENSSLLOCATION sha1 -binary | YOUROPENSSLLOCATION base64
+enter your keystore password , If you enter wrong password it will give wrong KeyHash
+
+NOTE
+
+If for some reason if it gives error at some path then wrap that path in double quotes .Also Windows power shell was not working well for me, I used git bash (or use command prompt) .
+
+example
+
+keytool -exportcert -alias androiddebugkey -keystore "C:\Users\Redman\.android\debug.keystore" | "C:\openssl-0.9.8k_X64\bin\openssl" sha1 -binary | "C:\openssl-0.9.8k_X64\bin\openssl" base64
--- app/src/main/java/com/bss/uis/ui/SplashActivity.java
+++ app/src/main/java/com/bss/uis/ui/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -17,17 +18,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bss.uis.R;
 import com.bss.uis.constant.AppConstants;
-import com.bss.uis.ui.navDrawer.DrawerMainActivity;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
+    private CallbackManager callbackManager;
     ImageView logoview;
     TextView copyright;
     private LoginButton fLoginBtn;
@@ -55,8 +64,41 @@ public class SplashActivity extends AppCompatActivity {
         fLoginBtn.setReadPermissions(Arrays.asList(EMAIL));
         logoview = findViewById(R.id.imageView);
         copyright = findViewById(R.id.copyright);
+        callbackManager = CallbackManager.Factory.create();
         Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation);
         logoview.startAnimation(animation);
+        fLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            public void onSuccess(final LoginResult loginResult) {
+                Log.i(TAG, "onSuccess: logged in successfully");
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.i(TAG, "onCompleted: response: " + response.toString());
+                        try {
+                            String email = object.getString("email");
+                            String userId = object.getString("id");
+                            String name = object.getString("name");
+                            handleAccessToken(loginResult.getAccessToken().getToken(), email, userId, name, "Facebook");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -69,8 +111,8 @@ public class SplashActivity extends AppCompatActivity {
                 anim.setDuration(AppConstants.ANIMATION_TIMEOUT);
                 GoogleCustomll.startAnimation(anim);
                 fbCustombuttonll.startAnimation(anim2);
-                Intent intent = new Intent(SplashActivity.this, DrawerMainActivity.class);
-                startActivity(intent);
+              //  Intent intent = new Intent(SplashActivity.this, DrawerMainActivity.class);
+               // startActivity(intent);
                 //invoke the SecondActivity.
 
                 finish();
@@ -90,5 +132,14 @@ public class SplashActivity extends AppCompatActivity {
             Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
             startActivityForResult(intent, RC_SIGN_IN);
         }
+    }
+    private void handleAccessToken(String token, String email, String userId, String name, String source) {
+        System.out.println(token);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
